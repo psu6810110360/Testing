f6f067a32996c7079d615ff73320272054a23f4d
psu6810110360 <supakorn.pant@gmail.com>
2026-02-15 12:08:59 +0700

    funny2

--- TestCode/test_funny_string.py
+++ TestCode/test_funny_string.py
@@ -3,9 +3,13 @@ from ProductCode.funny_string import funny_string
 
 class FunnyStringTest(unittest.TestCase):
     def test_give_acxz_should_be_funny(self):
-        # Arrange
+        
         s = "acxz"
-        # Act
+        
         result = funny_string(s)
-        # Assert
-        self.assertEqual(result, "Funny") # [cite: 31]
+        
+        self.assertEqual(result, "Funny")
+    def test_give_bcxz_should_be_not_funny(self):
+        s = "bcxz"
+        result = funny_string(s)
+        self.assertEqual(result, "Not Funny")
