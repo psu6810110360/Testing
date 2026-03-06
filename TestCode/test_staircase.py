90fd17addf32773e6df37a25610330f00849e088
psu6810110360 <supakorn.pant@gmail.com>
2026-03-06 20:32:13 +0700

    more_stair

--- TestCode/test_staircase.py
+++ TestCode/test_staircase.py
@@ -1,14 +1,21 @@
 import unittest
 from ProductCode.staircase import staircase
 
-class StaircaseTest(unittest.TestCase): 
+
+class StaircaseTest(unittest.TestCase):
     def test_give_n_2_should_be_staircase_2_layers(self):
         n = 2
-        pattern = '#'
+        pattern = "#"
         expected_output = " #\n##"
-        
-      
+
         result = staircase(n, pattern)
-        
-        
-        self.assertEqual(result, expected_output) 
+
+        self.assertEqual(result, expected_output)
+
+    def test_give_n_0_should_return_error(self):
+
+        self.assertEqual(staircase(0, "#"), "Out of range")
+
+    def test_give_n_31_should_return_error(self):
+
+        self.assertEqual(staircase(31, "#"), "Out of range")
