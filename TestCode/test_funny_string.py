82542d15c6edcc89e61ae8a385f331a918fa5df6
psu6810110360 <supakorn.pant@gmail.com>
2026-03-06 19:59:52 +0700

    funny5case

--- TestCode/test_funny_string.py
+++ TestCode/test_funny_string.py
@@ -1,19 +1,28 @@
 import unittest
 from ProductCode.funny_string import funny_string
 
+
 class FunnyStringTest(unittest.TestCase):
     def test_give_acxz_should_be_funny(self):
-        
+
         s = "acxz"
-        
+
         result = funny_string(s)
-        
+
         self.assertEqual(result, "Funny")
+
     def test_give_bcxz_should_be_not_funny(self):
         s = "bcxz"
         result = funny_string(s)
         self.assertEqual(result, "Not Funny")
+
     def test_give_aaaaa_should_be_funny(self):
         self.assertEqual(funny_string("aaaaa"), "Funny")
+
     def test_give_length_2_string_should_be_funny(self):
         self.assertEqual(funny_string("ab"), "Funny")
+
+    def test_give_lmnop_should_be_funny(self):
+        s = "lmnop"
+        result = funny_string(s)
+        self.assertEqual(result, "Funny")
