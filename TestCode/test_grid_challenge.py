ac3a3640035254cec695f966b4002cf7b7c834c8
psu6810110360 <supakorn.pant@gmail.com>
2026-03-06 20:25:52 +0700

    more_grid

--- TestCode/test_grid_challenge.py
+++ TestCode/test_grid_challenge.py
@@ -10,3 +10,12 @@ class GridChallengeTest(unittest.TestCase):
     def test_give_unsorted_columns_should_be_NO(self):
         grid = ["ebg", "afe", "hig"]
         self.assertEqual(grid_challenge(grid), "NO")
+
+    def test_give_single_row_YES(self):
+        self.assertEqual(grid_challenge(["z"]), "YES")
+
+    def test_give_identical_rows_YES(self):
+        self.assertEqual(grid_challenge(["abc", "abc", "abc"]), "YES")
+
+    def test_give_reverse_rows_that_need_sort_YES(self):
+        self.assertEqual(grid_challenge(["cba", "fed"]), "YES")
