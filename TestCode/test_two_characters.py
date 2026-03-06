1e3b025ef1d0bca8f182fdf2254358b7e9029539
psu6810110360 <supakorn.pant@gmail.com>
2026-03-06 20:24:08 +0700

    more_twochar

--- TestCode/test_two_characters.py
+++ TestCode/test_two_characters.py
@@ -6,3 +6,15 @@ from ProductCode import two_characters
 class TwoCharactersTest(unittest.TestCase):
     def test_give_beabeefeab_should_be_5(self):
         self.assertEqual(two_characters("beabeefeab"), 5)
+
+    def test_give_aaaaa_should_be_0(self):
+        self.assertEqual(two_characters("aaaaa"), 0)
+
+    def test_give_aabb_should_be_0(self):
+        self.assertEqual(two_characters("aabb"), 0)
+
+    def test_give_empty_string_should_be_0(self):
+        self.assertEqual(two_characters(""), 0)
+
+    def test_give_aba_should_be_3(self):
+        self.assertEqual(two_characters("aba"), 3)
